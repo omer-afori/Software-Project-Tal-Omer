9fbb1996a92d1a4a7f7fb1826ca4b4d06e8452dd
omer-afori <omerafori@mail.tau.ac.il>
2024-07-17 15:47:39 +0300

    print debug4

--- symnmf.c
+++ symnmf.c
@@ -318,9 +318,7 @@ int main(int argc, char *argv[]) {
     goal = argv[1];
     filename = argv[2];
     if (strncmp(goal, "sym", 4) == 0){
-        printf("TEST1\n");
         head = initialize(filename);
-        printf("TEST2\n");
         if (sym_wrapper(head, 0, n, d) == NULL){
             free_matrix(head, n);
             printf(error_msg);
@@ -347,7 +345,6 @@ int main(int argc, char *argv[]) {
         printf(error_msg);
         exit(-1);
     }
-    printf("TEST3\n");
     free_matrix(head, n);
     return 1;
 }
